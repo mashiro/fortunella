7de167a3dd907eaaa2b58dfd9fd6d71ac7efd69e
mashiro <y.mashiro@gmail.com>
2010-09-14 11:59:57 +0900

    modified startup script

--- fortunella.py
+++ fortunella.py
@@ -16,34 +16,40 @@ LOG_LEVELS = dict(
     error=logging.ERROR,
     critical=logging.CRITICAL)
 
-def wrapstdio():
+def setupenvs():
+    locale.setlocale(locale.LC_ALL, '')
     enc = sys.getfilesystemencoding()
     sys.stdin = codecs.getreader(enc)(sys.stdin, 'replace')
     sys.stdout = codecs.getwriter(enc)(sys.stdout, 'replace')
 
-def setuplogger(config):
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s [%(name)s] %(message)s', '%Y-%m-%d %H:%M:%S')
-    stdout_handler.setFormatter(formatter)
-    logger = logging.getLogger('fortunella')
-    logger.addHandler(stdout_handler)
-    loglevel = config['general'].get('loglevel', '')
-    logger.setLevel(LOG_LEVELS.get(loglevel, logging.INFO))
+def setuplogger(general):
+    default_logger = logging.getLogger('fortunella')
+    default_logger.setLevel(logging.DEBUG)
+    default_formatter = logging.Formatter('%(asctime)s %(levelname)s [%(name)s] %(message)s', '%Y-%m-%d %H:%M:%S')
+    default_level = LOG_LEVELS.get(general.get('loglevel'), logging.INFO)
+
+    def addhandler(handler, logger=default_logger, formatter=default_formatter, level=default_level):
+        handler.setFormatter(formatter)
+        handler.setLevel(level)
+        logger.addHandler(handler)
+
+    addhandler(logging.StreamHandler(sys.stdout))
+    #addhandler(logging.FileHandler('log.txt', mode='a', encoding='utf-8'))
 
 def main(config):
-    setuplogger(config)
+    setuplogger(config.get('general', {}))
     fortunella.core.run(config)
 
 if __name__ == '__main__':
-    locale.setlocale(locale.LC_ALL, '')
-    wrapstdio()
+    try:
+        setupenvs()
 
-    parser = optparse.OptionParser()
-    parser.add_option('-c', '--config', dest='config', default='config.yaml', help='config filename')
-    options, args = parser.parse_args()
+        parser = optparse.OptionParser()
+        parser.add_option('-c', '--config', dest='config', default='config.yaml', help='config filename')
+        options, args = parser.parse_args()
 
-    config = yaml.load(open(options.config))
-    general = config.setdefault('general', {})
-
-    main(config)
+        config = yaml.load(open(options.config))
+        main(config)
+    except KeyboardInterrupt:
+        pass
 
